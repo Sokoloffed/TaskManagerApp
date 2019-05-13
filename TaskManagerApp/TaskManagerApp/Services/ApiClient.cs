a4fda427a8fb85dc9310273bf342146fec335fe7
unknown <dansru@gmail.com>
2019-05-13 18:49:24 +0300

    Backup for windows

--- TaskManagerApp/TaskManagerApp/Services/ApiClient.cs
+++ TaskManagerApp/TaskManagerApp/Services/ApiClient.cs
@@ -8,7 +8,7 @@ namespace TaskManagerApp.Serivces
 {
     public class ApiClient
     {
-        public string apiUri = @"http://d02d037e.ngrok.io/api/";
+        public string apiUri = @"http://28f945bf.ngrok.io/api/";
         private static HttpClient httpClient = new HttpClient();
 
         public ApiClient()
